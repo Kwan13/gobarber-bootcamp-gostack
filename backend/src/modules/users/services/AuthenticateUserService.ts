790b122e114142c37150c0e3cbadf066d0a43f10
Kwan13 <kawan-silva@outlook.com>
2020-11-04 17:05:23 -0300

    testing the application with "jest"

---
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,61 @@
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
+import { injectable, inject } from 'tsyringe';
+
+// Error
+import AppError from '@shared/errors/AppError';
+
+// Models
+import User from '../infra/typeorm/entities/User';
+
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private HashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ email, password }: Request): Promise<Response> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const passwordMatched = await this.HashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination', 401);
+    }
+
+    const { expiresIn, secret } = authConfig.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return { user, token };
+  }
+}
+
+export default AuthenticateUserService;
---
+++ backend/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,46 @@
+import { inject, injectable } from 'tsyringe';
+
+// Error
+import AppError from '@shared/errors/AppError';
+
+// Models
+// import User from '../infra/typeorm/entities/User';
+
+import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokensRepository from '../repositories/IUsersTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('userTokensRepository')
+    private userTokensRepository: IUserTokensRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists');
+    }
+
+    await this.userTokensRepository.generate(user.id);
+
+    this.mailProvider.sendMail(
+      email,
+      'Pedido de recuperação de senha recebido.',
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
