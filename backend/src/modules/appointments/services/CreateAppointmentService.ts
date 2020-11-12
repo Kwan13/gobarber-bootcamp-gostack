48409e04aa262ec4254364be08e439112b6813c3
Kwan13 <kawan-silva@outlook.com>
2020-11-12 16:33:16 -0300

    service scheduling

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -12,6 +12,7 @@ import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface Request {
   provider_id: string;
+  user_id: string;
   date: Date;
 }
 
@@ -22,7 +23,11 @@ class CreateAppointmentService {
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
-  public async execute({ provider_id, date }: Request): Promise<Appointment> {
+  public async execute({
+    provider_id,
+    date,
+    user_id,
+  }: Request): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
     const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
@@ -35,6 +40,7 @@ class CreateAppointmentService {
 
     const appointment = await this.appointmentsRepository.create({
       provider_id,
+      user_id,
       date: appointmentDate,
     });
 
